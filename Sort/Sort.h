0d24db698e6907a322731b8bbaef7229b70fe3d1
bigbatmoegan <49653903+bigbatmoegan@users.noreply.github.com>
2019-04-23 15:48:02 -0400

    Moving from School repo to personal repo

---
+++ Sort/Sort.h
@@ -0,0 +1,18 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor 
+// received any help on this assignment.
+//
+// jeremorg
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <vector>
+
+class Sort
+{
+	public:
+		virtual void sort(std::vector<int> vector)=0;
+};
+
+#endif
